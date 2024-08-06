9b5fe3a66c43cbf6096fad310ed2f8d10425bc78
Aba Yeboah Dadzie <ADadzie@CORENET.LOCAL>
2024-08-06 10:51:07 +0100

    The main App for my fastAPI

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from users.api import user_router
 from tasks.api import task_router
 
 
 app = FastAPI(
-    title="Task Management System",
+    title="Task Management System fastAPI",
     description="This is the main app that includes all the APIs",
     version="1.0",
 )
+
+template = Jinja2Templates(directory="templates")
 
 app.include_router(user_router, prefix="/users", tags=["users"])
 app.include_router(task_router, prefix="/tasks", tags=["tasks"])
